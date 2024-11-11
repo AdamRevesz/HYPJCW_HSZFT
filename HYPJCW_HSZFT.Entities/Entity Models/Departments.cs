4735c2fef016f99ae78bb48d807e330687ab5cac
Adam Revesz <revike6@gmail.com>
2024-11-11 16:48:01 +0100

    Created The class Employee and Departments

---
+++ HYPJCW_HSZFT.Entities/Entity Models/Departments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HYPJCW_HSZFT.Entities.Entity_Models
+{
+    public class Departments
+    {
+        [MaxLength(200)]
+        public string Name { get; set; }
+        [Key]
+        public string DepartmentCode { get; set; }
+        [MaxLength(200)]
+        public string HeadOfDepartment { get; set; }
+
+        public Departments(string name, string departmentCode,string headOfDepartment )
+        {
+            Name = name;
+            DepartmentCode = departmentCode;
+            HeadOfDepartment = headOfDepartment;
+        }
+    }
+}
---
+++ HYPJCW_HSZFT.Entities/Entity Models/Employee.cs
@@ -0,0 +1,66 @@
+﻿using HYPJCW_HSZFT.Entities.Dependencies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HYPJCW_HSZFT.Entities.Entity_Models
+{
+    public class Employee
+    {
+        [StringLength(50)]
+        [Key]
+        public string EmployeeId { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
+        [Range(1960,9999)]
+        public static int BirthYear { get; set; }
+        [Range(1000,9999)]
+        public int StartYear { get; set; }
+        public int CompletedProjects { get; set; }
+        public bool Active { get; set; }
+        public bool Retired { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Job { get; set; }
+        public string Level { get; set; }
+        public int Salary { get; set; }
+        public int Commission { get; set; }
+        public List<Departments> Departments { get; set; }
+
+        public Employee(
+        string employeeId,
+        string name,
+        int birthYear,
+        int startYear,
+        int completedProjects,
+        bool active,
+        bool retired,
+        string email,
+        string phone,
+        string job,
+        string level,
+        int salary,
+        int commission,
+        List<Departments> departments = null
+    )
+        {
+            EmployeeId = employeeId;
+            Name = name;
+            BirthYear = birthYear;
+            StartYear = startYear;
+            CompletedProjects = completedProjects;
+            Active = active;
+            Retired = retired;
+            Email = email;
+            Phone = phone;
+            Job = job;
+            Level = level;
+            Salary = salary;
+            Commission = commission;
+            Departments = departments ?? new List<Departments>(); // Initialize with empty list if null
+        }
+    }
+}
